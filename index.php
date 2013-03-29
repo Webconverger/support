68b27be97b8e0a959c754dbdc54e2ba745376455
Kai Hendry <hendry@webconverger.com>
2013-03-29 15:21:58 +0800

    support site

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html>
- <head>
-  <meta charset="utf-8" />
-  <title>Webconverger QA</title>
-  <style>
-  body { margin: 2em; font-size: 1.2em; font-family: sans-serif; text-align: center; background-color: white; }
-  input { font-size: 1.1em; }
-  form { border: thin solid silver; padding: 1em; background: #CCC url(http://webconverger.com/img/linen_light.jpg); -moz-border-radius: 1em; -webkit-border-radius: 1em; border-radius: 1em; }
-</style>
-</head>
-<body>
+<?php
 
-<img src="http://webconverger.com/img/logo.png">
+$apachelogs = "a";
+$logdir = "l";
+date_default_timezone_set('Europe/Berlin');
+umask(002);
 
-<form action="qa.php" method="post" enctype="multipart/form-data">
-<input name="f" type="file" />
-<input value="Upload" type="submit">
-</form>
+if (isset($_FILES['f']) && is_uploaded_file($_FILES['f']['tmp_name'])) {
+	$f = str_replace("%", ";", $_FILES['f']['name']);
+	$dir = $logdir;
+	if(! file_exists("$apachelogs/" . $f)) {
+		$msg = "Are you a subscriber?\n";
+		$msg .= "http://config.webconverger.com/clients/install-config/$f\n";
+		$msg .= "Please email support@webconverger.com referencing " . $f . "\n";
+		echo($msg);
+		$dir = 'fail';
+	}
+	$dir = $dir . '/' . $f . '/' . date("U");
+	mkdir($dir, 0777, true);
+	move_uploaded_file($_FILES["f"]['tmp_name'], $dir . '/log.tar.bz2');
+	`cd "$dir"; tar jxvf log.tar.bz2 --strip=2`;
+} elseif (isset($_REQUEST["q"])) {
+	$id = basename($_REQUEST["q"]);
+	if(! file_exists("$apachelogs/" . $id)) {
+		$msg = "Could not find subscriber logs.\n";
+		die($msg);
+	}
+	include "support.php";
+} else {
+	// Replace with some Ad?
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+	die("404");
+}
 
-
-<h3>Can't upload large files? Adjust post_max_size and upload_max_filesize to = 50M in php.ini</h3>
-<pre>
-~$ grep -E 'filesize|max_size' $(php --ini | head -n1 | awk '{print $NF}')
-</pre>
-
-<h3>Upload UUID.log file like so</h3>
-<pre>curl -F "f=@015BEE48-1351-CB11-9575-F8DF31C806FA.log" http://<?php echo $_SERVER["HTTP_HOST"]; ?>/qa.php</pre>
-<p>Which is put in YYYY-MM-DD/015BEE48-1351-CB11-9575-F8DF31C806FA.log</p>
-
-<h3>TODO</h3>
-
-<p>Only move into place UUIDs we know about (customers pay for). Currently it's unsecured.</p>
-<p>Protect YYYY-MM-DD directories.</p>
-
-</body>
-</html>
+?>
---
+++ support.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8" />
+  <title>Support logs for <?php echo $id ?></title>
+  <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+<div class="content">
+<img src="http://webconverger.com/img/logo.png" alt=Webconverger class=headlogo width=450 height=50 />
+
+<h1>Apache logs</h1>
+
+<p>Logs from when a configuration is fetched from config.webconverger.com</p>
+
+<p>
+<a href="/a/<?php echo $id;?>">Full log for <?php echo $id;?></a>
+</p>
+
+<pre>
+<?php echo `tail "a/$id"`; ?>
+</pre>
+
+<h1>Logs</h1>
+
+<p>If the <code>cron=0%20*/5%20*%20*%20*%20support</code> debug cronjob is enabled in your <a href="http://config.webconverger.com/clients/install-config/<?php echo $id; ?>">configuration</a>, logs will be published here.</p>
+
+<?php
+if (is_dir("$logdir/$id") && $handle = opendir("$logdir/$id")) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "<li><a href=/$logdir/$id/$entry/syslog>". date('c', intval($entry)) . "</a></li>\n";
+        }
+    }
+    closedir($handle);
+}
+?>
+
+
+
+<hr>
+<footer>
+<p>Questions our <a href="http://config.webconverger.com/faq"><abbr title="Frequently Asked Questions">FAQ</abbr></a> didn't answer? Please email <a href="&#x6D;&#97;&#x69;&#108;&#x74;&#111;:&#x73;&#97;&#x6C;&#101;&#x73;&#64;&#119;&#x65;&#x62;&#99;&#111;&#x6E;&#118;&#101;&#114;&#x67;&#101;&#x72;&#46;&#x63;&#111;&#x6D;">&#x73;&#97;&#x6C;&#101;&#x73;&#64;&#119;&#x65;&#x62;&#99;&#111;&#x6E;&#118;&#101;&#114;&#x67;&#101;&#x72;&#46;&#x63;&#111;&#x6D;</a>.</p>
+<p>&copy; Webconverger <?php echo date('Y'); ?></p>
+
+</footer>
+
+</div> <!-- container -->
+</div> <!-- content -->
+
+</body>
+</html>
+
